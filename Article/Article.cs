623c7e731324a3a8fbad9df6423f9ae80d517e5f
StephenGodard <stephen.godard@edu.itescia.fr>
2020-09-22 13:52:23 +0200

    Article exercice

---
+++ Article/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Articlee
+{
+    public struct Article
+    {
+        public Article(string name, double prix, int quantite)
+        {
+            this.Nom = name;
+            this.Prix = prix;
+            this.Quantite = quantite;
+        }
+
+        public string Nom { get; set; }
+        public double Prix { get; set; }
+        public int Quantite { get; set; }
+
+        public string Infos()
+        {
+            return  $"Nom : {Nom}, Prix : {Prix}, Quantite : {Quantite}";
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine(this.Infos());
+        }
+
+        public void Ajouter(int add)
+        {
+            Quantite += add;
+        }
+
+        public void Retirer(int remove)
+        {
+            Quantite -= remove;
+        }
+    }
+}
---
+++ Article/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Articlee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article a1 = new Article("Article 1", 3, 10);
+            a1.Afficher();
+            a1.Ajouter(5);
+            a1.Retirer(2);
+            a1.Afficher();
+        }
+    }
+}
