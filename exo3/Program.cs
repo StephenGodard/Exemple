4d3db3bb62ce71dec5916d94fe9772925702b0f3
StephenGodard <stephen.godard@edu.itescia.fr>
2020-09-22 14:15:06 +0200

    exo 3

---
+++ exo3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article a1 = new Article("Article 1", 3, 10,Type.alimentaire);
+            a1.Afficher();
+            a1.Ajouter(5);
+            a1.Retirer(2);
+            a1.Afficher();
+        }
+    }
+}
---
+++ exo3/type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exo3
+{
+   public enum Type
+    {
+        alimentaire,
+        droguerie,
+        habillement,
+        loisir
+    }
+}
