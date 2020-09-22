e8bc2fa6ddb6d7820467a3dcbba4265783d06a54
StephenGodard <stephen.godard@edu.itescia.fr>
2020-09-22 15:30:05 +0200

    exercicePersonne

---
+++ exercicePersonne/Personne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicePersonne
+{
+    class Personne
+    {
+        private string nom { get; set; }
+        private string prenom { get; set; }
+        private int age { get; set; }
+        static int nombrePersonne { get; set; }
+        public Personne(string nom,string prenom,int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            nombrePersonne++;
+        }
+        public void afficher()
+        {
+            Console.WriteLine($"nom:{nom},prenom:{prenom}, age={age}");
+
+        }
+        public void Combien()
+        {
+            Console.WriteLine(nombrePersonne + " personnes ont été crées depuis le début");
+        }
+    }
+}
---
+++ exercicePersonne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercicePersonne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne a1 = new Personne("Godard", "Stephen", 24);
+            a1.afficher();
+            a1.Combien();
+        }
+    }
+}
