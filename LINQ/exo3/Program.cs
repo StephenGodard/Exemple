2a634c87f0af551922466b33c79ad66e917dae02
StephenGodard <stephen.godard@edu.itescia.fr>
2020-09-24 16:51:23 +0200

    LINQexo3

---
+++ LINQ/exo3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqExo1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+           var maListe = new List<int>() {20,21,22,23,24,25,26,27,28,29,30};
+
+            var division = maListe.Where(num => num % 5 == 0);
+
+            foreach (var num in division)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+        }
+    }
+}
+    
+
