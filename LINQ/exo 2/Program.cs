4af42c361b1661b51f8f8d62a48cfd67705c2db1
StephenGodard <stephen.godard@edu.itescia.fr>
2020-09-24 16:15:06 +0200

    exo 2 LINQ

---
+++ LINQ/exo 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqExo1
+{
+    class Program
+    {
+        public static bool isBissextile(int year)
+        {
+            var firstDate = new DateTime(year, 1, 1);
+            var endDate = new DateTime(year, 12, 31);
+            TimeSpan Ts = endDate - firstDate;
+
+            var nbJours = Ts.TotalDays + 1;
+            if (nbJours == 366)
+                return true;
+
+            return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> maListe = new List<int> {2012, 2014, 2015, 2016, 2017, 2018, 2019, 2020 };
+
+
+            var requeteBissextile = from uneAneee in maListe
+                                    where isBissextile(uneAneee)
+                                    select uneAneee;
+
+
+            List<int> anneesBissextiles = requeteBissextile.ToList();
+            foreach (int annee in anneesBissextiles)
+            {
+                Console.Write(annee + "\n");
+            }
+        }
+
+
+
+
+    }
+    }
+
